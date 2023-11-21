0c3ec6b902b5484af88917e6ccfca8dd497daf8a
Alexander Winkowski <dereference23@outlook.com>
2023-11-21 15:45:40 +0000

    AK3: Add support for overriding cmdline

--- tools/ak3-custom.sh
+++ tools/ak3-custom.sh
@@ -1,2 +1,9 @@
 ### AnyKernel custom methods
 ## dereference23@github.com
+
+check_cmdline() {
+  if [ -f $AKHOME/cmdline ]; then
+    sed -i '/^cmdline/d' $SPLITIMG/header;
+    echo cmdline=$(tr '\n' ' ' < $AKHOME/cmdline) >> $SPLITIMG/header;
+  fi;
+}
