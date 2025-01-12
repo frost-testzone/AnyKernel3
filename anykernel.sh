50725be385369f3a13fe1a78a76abfec4e1c90f2
Alexander Winkowski <dereference23@outlook.com>
2025-01-12 06:19:09 +0000

    AK3: Initialize custom methods

--- anykernel.sh
+++ anykernel.sh
@@ -19,6 +19,7 @@ IS_SLOT_DEVICE=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
+. tools/ak3-custom.sh;
 
 # boot install
 split_boot;
