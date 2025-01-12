083c2ca2717e975a9c9e7082f9bd28ba98edfd73
Alexander Winkowski <dereference23@outlook.com>
2025-01-12 05:59:24 +0000

    anykernel: Clean up properties

--- anykernel.sh
+++ anykernel.sh
@@ -4,20 +4,10 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=
 do.devicecheck=1
-do.modules=0
-do.systemless=1
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
-supported.patchlevels=
-supported.vendorpatchlevels=
+device.name1=
 '; } # end properties
 
 
